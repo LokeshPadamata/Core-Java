1eeaa4a06edaf5847883ec6b587db1acbc951040
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-04-23 22:06:24 +0530

    Java

---
+++ Collectionframeworks_SetInterface/src/setinterface/TreeSetEx.java
@@ -0,0 +1,20 @@
+package setinterface;
+import java.util.TreeSet;
+public class TreeSetEx {        // An Example of Sorted Set
+
+	public static void main(String[] args) {
+		TreeSet<Integer> treeset = new TreeSet<Integer>();
+		treeset.add(100);
+		treeset.add(200);
+		treeset.add(50);
+		treeset.add(200);
+		treeset.add(2);
+		treeset.add(200);
+		treeset.add(77);
+		treeset.add(20);
+		treeset.add(0);
+		System.out.println(treeset);
+
+	}
+
+}
