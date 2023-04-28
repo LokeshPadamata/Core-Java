519212a0f2ac7fa3c7569164881152af8b723bb0
LokeshPadamata <127922284+LokeshPadamata@users.noreply.github.com>
2023-04-28 12:03:20 +0530

    Delete ATMtransactionApp directory

--- ATMtransactionApp/src/atmtransactionapp/CustomerCredentials.java
+++
@@ -1,7 +0,0 @@
-package atmtransactionapp;
-public class CustomerCredentials {
-	String nameOnCard="Lokesh Padamata";
-	String bankname="State Bank of India";
-	int pin=1324;
-	long acbalance = 9000;
-}
