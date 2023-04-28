fbc365d349254ce7536669f2255a9c0bae1e9340
LokeshPadamata <lokeshpadamata123@gmail.com>
2023-04-28 12:26:27 +0530

    Polymorphism

---
+++ Polymorphism/src/polymorphism/MethodOverRiding.java
@@ -0,0 +1,27 @@
+package polymorphism;
+
+class A1{
+	void m1() {
+		System.out.println("Calling m1 method in A");
+	}
+	void m2() {
+		System.out.println("Calling m2 method in A");
+	}
+}
+class B1 extends A1{
+	void m2() {
+		System.out.println("Calling m2 method in B");
+	}
+}
+public class MethodOverRiding {
+
+	public static void main(String[] args) {
+		//B1 b = new B1();
+		//b.m2();
+		//b.m1();
+		A1 a = new B1();  //Dynamic Polymorphism
+		a.m2();
+	}
+
+}
+
